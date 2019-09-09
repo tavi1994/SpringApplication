d82c890649b3eec5c82055527ff5ca8e78828af8
Bartek <bartekmarcysiak@gmail.com>
2019-09-09 10:57:09 +0100

    Working First Version

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -3,18 +3,13 @@ package com.qa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- *
- */
-@SpringBootApplication
 
+@SpringBootApplication
 public class App
 {
     public static void main( String[] args )
 
     {
         SpringApplication.run(App.class, args);
-        System.out.println( "Hello World!" );
     }
 }
